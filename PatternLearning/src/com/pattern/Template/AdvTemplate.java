f8cf6e7c2be82c6ac7264901ece13284bbcd2667
haozai309 <haozhanghao_87@163.com>
2015-03-21 22:07:48 +0800

    template pattern: basic version without thread

---
+++ PatternLearning/src/com/pattern/Template/AdvTemplate.java
@@ -0,0 +1,17 @@
+package com.pattern.Template;
+
+public class AdvTemplate {
+
+    private String mAdvSubject = "XXX bank October credit cart activity";
+
+    private String mAdvContext = "The October activity notice, once you use your credit card, "
+            + "a bonus of $100 is yours";
+
+    public String getAdvSubject() {
+        return this.mAdvSubject;
+    }
+
+    public String getAdvContext() {
+        return this.mAdvContext;
+    }
+}
