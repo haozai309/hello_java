ec4e2593e75a421267ecf44fc5ecc9077cdf9a44
haozai309 <haozhanghao_87@163.com>
2015-04-10 08:07:23 +0800

    NetworkTest: use HttpURLConnection to get a web page

---
+++ NetworkTest/src/com/web/networktest/MainActivity.java
@@ -0,0 +1,124 @@
+package com.web.networktest;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+    public static final int SHOW_RESPONSE = 0;
+    private static final String TAG = "NetworkTest/MainActivity";
+
+    private Button mSendRequest;
+    private TextView mResponseText;
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            Log.e(TAG, "msg.what " + msg.what);
+            switch (msg.what) {
+            case SHOW_RESPONSE:
+                String response = (String) msg.obj;
+                mResponseText.setText(response);
+                break;
+            default:
+                Log.e(TAG, "default flow");
+                break;
+            }
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSendRequest = (Button) findViewById(R.id.send_request);
+        mResponseText = (TextView) findViewById(R.id.response);
+        mSendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.send_request) {
+            Log.i(TAG, "press button " + v.getId());
+            sendHttpRequest();
+        }
+    }
+
+    private void sendHttpRequest() {
+        Log.i(TAG, "sendHttpRequest+");
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL("http://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+
+                    Log.i(TAG, "connection is " + connection);
+
+                    InputStream input = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                    Log.i(TAG, "response is " + reader.toString());
+
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    Message message = new Message();
+                    message.what = SHOW_RESPONSE;
+                    message.obj = response.toString();
+                    mHandler.sendMessage(message);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+}
