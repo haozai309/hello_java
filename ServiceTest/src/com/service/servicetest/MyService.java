e6fc39f9998203fb8a5752d4c55fb7b4e5d4ac30
haozai309 <haozhanghao_87@163.com>
2015-04-14 14:34:09 +0800

    ServiceTest: use startForeground to increase priority

--- ServiceTest/src/com/service/servicetest/MyService.java
+++ ServiceTest/src/com/service/servicetest/MyService.java
@@ -1,5 +1,7 @@
 package com.service.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -35,8 +37,19 @@ public class MyService extends Service {
 
     @Override
     public void onCreate() {
-        Log.i(TAG, "onCreate");
         super.onCreate();
+        Log.i(TAG, "onCreate");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Notification comes")
+                .setContentText("Content text")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        Log.i(TAG, "start foreground");
     }
 
     @Override
