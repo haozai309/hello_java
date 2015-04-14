8b503851577ae2495eb47956cbad14427713bc85
haozai309 <haozhanghao_87@163.com>
2015-04-14 16:13:15 +0800

    NotificationTest: show usage of notification

---
+++ NotificationTest/src/com/media/notificationtest/MainActivity.java
@@ -0,0 +1,69 @@
+package com.media.notificationtest;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private Button mSendNotice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSendNotice = (Button) findViewById(R.id.send_notice);
+        mSendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.send_notice:
+            NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+            Intent intent = new Intent(this, NotificationActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+            Notification notification = new Notification.Builder(this)
+                    .setContentTitle("Notification comes")
+                    .setContentText("Content text")
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .build();
+            notificationManager.notify(1, notification);
+            break;
+
+        default:
+            break;
+        }
+
+    }
+}
